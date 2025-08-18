0c302ad935c99e9ca6628ba56cd47305415f6ad3
andreyindras <andreyindras55@gmail.com>
2025-08-18 15:49:47 -0300

    estrutura e impressão xadrez

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
