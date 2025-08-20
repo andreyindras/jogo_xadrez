7798e4f1941844629244a3f01fd40cf8ddf5201e
andreyindras <andreyindras55@gmail.com>
2025-08-20 11:49:51 -0300

    mover peças

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,28 @@
 package org.example;
 
 import org.example.chess.ChessMatch;
+import org.example.chess.ChessPiece;
+import org.example.chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
 
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("source: ");
+            ChessPosition source = UI.readChessposition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessposition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
