11cd31a5faaff6c36abbdb0bee1bb78c9f3d6dba
andreyindras <andreyindras55@gmail.com>
2025-08-25 15:37:01 -0300

    tratemento melhorado para promoção

--- src/main/java/org/example/chess/ChessMatch.java
+++ src/main/java/org/example/chess/ChessMatch.java
@@ -5,7 +5,6 @@ import org.example.boardgame.Piece;
 import org.example.boardgame.Position;
 import org.example.chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -114,7 +113,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
